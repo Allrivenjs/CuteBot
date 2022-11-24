e429886e3658d3fb97e84412072a26a1ace58df0
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2022-11-23 23:31:00 -0500

    Update ping.js

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     DESCRIPTION: 'A simple command that does nothing',
     PERMISSION: ['Administrator', 'KickMembers', 'BanMembers'],
-    OWNER: true,
+    OWNER: false,
     async execute(client, message, args, prefix) {
         return message.reply(`\`${client.ws.ping}ms\``);
     }
